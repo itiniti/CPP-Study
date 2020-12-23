a54836c85bbe4cd9e45a75beb14eef5a8a3f5aa1
itiniti <mack4ford@gmail.com>
2020-12-23 09:51:52 +0900

    Add files via upload

---
+++ Study/002.Get Average With Structure/002.Get Average With Structure/Untitled1.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+typedef struct student {
+	int score;
+};
+
+//student 배열의 최소값
+int GetMinScore(student* students, int n) {
+	int Index = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (students[i].score < students[Index].score) {
+			Index = i;
+		}
+	}
+	return students[Index].score;
+}
+
+//student 배열의 최대값
+int GetMaxScore(student* students, int n) {
+	int Index = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (students[i].score > students[Index].score) {
+			Index = i;
+		}
+	}
+	return students[Index].score;
+}
+
+//students 배열의 평균 구하기
+int GetAverage(student* students, int n) {
+	int sum=0;
+	for (int i=0;i<n;i++)
+		sum+=students[i].score;
+	
+	int average=sum/n;
+	return average;
+}
+
+//students 배열의 크기 구하기
+int GetSize(student* students, int n){
+	
+}
+
+int main(void) {
+	//총 학생 수 입력 받기
+	int nStudentsCount = 0;
+	cout << "총 학생 수를 입력해 주세요 : ";
+	cin >> nStudentsCount;
+
+	//총 학생 수만큼 배열 동적 할당
+	student* Students = new student[nStudentsCount]; 
+
+	//학생 수 만큼 자료 입력 받기.
+	for (int i = 0; i < nStudentsCount; i++)
+	{
+		
+		cout << i + 1 << "번째 학생의 점수 : ";
+		cin >> Students[i].score;
+	}
+	cout << "------------------------------------------------------------"<<endl;
+	cout << "최저 점수 : " << GetMinScore(Students, nStudentsCount) << endl << "최대 점수 : " << GetMaxScore(Students, nStudentsCount) << endl << "평균 점수 : " << GetAverage(Students, nStudentsCount) << endl<<// " 총 학생 수 : " << sizeof(student)/sizeof(int)//;
+	//sizeof 를 이용해보려 했지만 잘안됌  
+}
