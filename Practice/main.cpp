f189fd0309c5cfc6149428d482fdbf99f961b01f
itiniti <mack4ford@gmail.com>
2020-12-24 10:11:03 +0900

    201224 strcut  /  memory address

--- Practice/main.cpp
+++ Practice/main.cpp
@@ -1,58 +1,42 @@
 #include <iostream>
-// Dev C++ 이라 일단 제외 #include "employeestruct.h"
 using namespace std;
 
-
-
-int main(void) {
-	struct Employee {
-		char firstInitial;
-		char lastInitial;
-		int employeeNumber;
-		int salary;
-		void print()
-		{
-				cout << "Employee : " << firstInitial << lastInitial << endl << "Number : " << employeeNumber << endl << "Salary : " << salary << endl << endl;
+int main(){
+	
+	for(int i=0;i<10;i++){
+		for(int j=0;j<10;j++){
+			cout << &i << endl << &j << endl << endl; // 변수에 할당된 메모리 주소 변화 체크
+			
 		}
+	//	cout << &j << endl; 변수 해제로 불러오기 불가능 
+	}
+	
+	struct Grade{ // 구조체 선언 
+		int A;
+		int B;
+		int C;
+		int D;
+		int F;
+		
+		void appraisal(int a){
+			if(a>=90)cout<<"잘 했네 " << endl;
+			else if(a>=80)cout<<"쫌 했네 " << endl;
+			else if(a>=70)cout<<"하긴 했네 " << endl;
+			else if(a>=60)cout<<"안 했네 " << endl;
+			else cout<<"쫌 해라 " << endl;
+			 
+		}
+	
 	};
 	
-	//구조체 예시 / 사업장에서 고용인에 대한 정보를 구조체 방식으로 표현
-	Employee anEmployee;
-	anEmployee.firstInitial = 'M';
-	anEmployee.lastInitial = 'G';
-	anEmployee.employeeNumber = 42;
-	anEmployee.salary = 80000;
-
-	Employee anotEmployee;
-	anotEmployee.firstInitial = 'E';
-	anotEmployee.lastInitial = 'H';
-	anotEmployee.employeeNumber = 97;
-	anotEmployee.salary = 100000;
-
-	//구조체 출력 확인
-	/*
-	cout << "Employee : " << anEmployee.firstInitial << anEmployee.lastInitial << endl;
-	cout << "Number : " << anEmployee.employeeNumber << endl;
-	cout << "Salary : " << anEmployee.salary << endl;
-	*/
-	anEmployee.print();
-	anotEmployee.print();
+	Grade score; // 구조체  유형 생성 
+	score.A = 90;
+	score.B = 80;
+	score.C = 70;
+	score.D = 60;
+	score.F = 0;
 	
-	
-	/*
-	cout << "Employee : " << anotEmployee.firstInitial << anotEmployee.lastInitial << endl;
-	cout << "Number : " << anotEmployee.employeeNumber << endl;
-	cout << "Salary : " << anotEmployee.salary << endl;
-	*/
-	
-
-	//구조체 변수 값 변화 체크
-	/*
-	anEmployee.employeeNumber += 42;
-	cout << "Number : " << anEmployee.employeeNumber << endl;//구조체 안에 있는 변수 값은 변화가능
-	*/
-
-
-
+	int myscr;
+	cin >> myscr ;
+	score.appraisal(myscr);
 }
-
